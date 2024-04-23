be9eeaa2ee91734b583856b9ae445a9700b18d1f
KumarG23 <sharma.neal@yahoo.com>
2024-04-23 16:49:16 -0400

    workin on it

--- src/About.jsx
+++ src/About.jsx
@@ -1,13 +1,47 @@
 import { Link } from "react-router-dom"
+import { Left } from "./App"
+import famPic from '../src/assets/A728013B-A2EA-42AA-BDD6-EF6A63F6761F_1_105_c.jpeg'
 
+
+const Nav = () => {
+  return (
+    <nav className="nav d-flex justify-content-center text-center">
+              <ul className="fs-5 list-unstyled" id="navlist">
+                <li>
+                  <Link className="btn mt-3 fw-bold" id="icon" to='/about'>About</Link>
+                </li>
+                <li className="btn mt-3  fw-bold" id="icon" to='/experience'>Experience</li>
+                <li className="btn mt-3 fw-bold" id="icon" to='/projects'>Projects</li>
+                <li className="btn mt-3 fw-bold" id="icon" to='/blog'>Blog</li>
+              </ul>
+            </nav>
+  )
+}
+const RightSide = () => {
+  return (
+    <div className="d-flex col-12">
+      <div id="right-side">
+        <h1 id="about">About Me</h1>
+        <img src={famPic} id="pic" className="mx-auto d-block mb-5"></img>
+        <p id="par">I am an aspiring full stack software engineer. Just a few months ago I decided to take the plunge and learn about software development in a high intensity BootCamp. So far I am loving making cool things on the computer. I love my family above all things. They're my world and the reason I am working through this BootCamp. I love soccer, I have played all my life and have also done some coaching. I can't get enough. I also love cats, I have four of them. I have a saying that my father has told me all my life: Do what you love and the money will come. I hope this BootCamp will allow me to fall more in love with coding.
+</p>
+      </div>
+    </div>
+  )
+}
 function About() {
   return (
     <div className="p-5">
-      <Link to='/'>{'<- Back'}</Link>
-      <h1>About Page</h1>
+      <Nav className="d-flex"/>
+      <Link style={{color: "brown"}} to='/'>{'Home'}</Link>
+      <RightSide />
+
+      
+
     </div>
   )
 }
 
 
-export default About
+export default About;
+export const Navigation = Nav;
--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,47 @@
 import { Link } from "react-router-dom"
+//import { Link } from "App.css"
+import { Navigation } from './About'
+import myPic from '../src/assets/kumar-profile.png';
 
-const Title = () => {
+const LeftSide = () => {
   return (
-    <h1>
-      Hello World!
-    </h1>
+    <div id="container" className="d-flex flex column flex-md-row">
+      <div className="d-flex justify-content-center col-12">
+        <header>
+          <div id="left-side" className="sticky-md-top justify-content-center text-center">
+            <h1 className="fs-1 fw-bold d-flex justify-content-center" id="info">Neal Sharma</h1>
+            <h2 className="mt-3 mb-3 fs-2 fw-bold" id="info">Web Developer</h2>
+            <img src={myPic} id="pic"></img>
+            <p className="mt-5 fs-5" id="info">I build beautiful and creative web apps</p>
+            
+            <div id="icon-container" className="d-flex col justify-content-center text-center">
+              <ul className="mt-8 d-flex list-unstyled" id="socials">
+                <li className="mr-3 mb-2"><a href="https://github.com/KumarG23" target="_blank" id="icon"><i className="fab fa-github"></i></a></li>
+                <li className="mr-3 mb-0 ms-2"><a href="https://www.linkedin.com/in/neal-sharma23" target="_blank" id="icon"><i className="fab fa-linkedin"></i></a></li>
+                <li className="mr-3 mb-0 ms-2"><a href="https://www.goodreads.com/user/show/124681580-neal-sharma" target="_blank" id="icon"><i className="fab fa-goodreads"></i></a></li>
+                <li className="mr-3 mb-0 ms-2"><a href="https://twitter.com/KumarG2369" target="_blank" id="icon"><i className="fa-brands fa-x-twitter"></i></a></li>
+                <li className="mr-3 mb-0 ms-2"><a href="https://www.instagram.com/nealkushmarma/" target="_blank" id="icon"><i className="fa-brands fa-instagram"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </header>
+      </div>
+    </div>
+  
   )
 }
 
 function App() {
   return (
-    <div 
-      className="bg-primary h-100 p-5"
-      style={{ color: 'white' }}
-    >
-      <Link className="text-light" to='/about'>About</Link>
-      <Title />
+    <div>
+      <Navigation />
+    <div className="container-fluid d-flex align-items-center justify-content-center" id="appContainer">
+      <LeftSide />
+    </div>
     </div>
   )
 }
 
 
-export default App
+export default App;
+export const Left = LeftSide;
